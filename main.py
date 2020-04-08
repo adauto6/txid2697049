a306aadcaabe3fb6c67a5c3ce90b025670d0f4fa
Adauto Luis <adauto6@netscape.net>
2020-04-08 19:38:13 -0300

    Creating phylogenic tree using 348 genomes. It was used MAFFT tool to align all organism with 30 threads

--- main.py
+++ main.py
@@ -1,72 +1,87 @@
+from Bio.Alphabet import generic_dna 
+from Bio.Seq import Seq 
+from Bio.SeqRecord import SeqRecord 
+from Bio.Align import MultipleSeqAlignment 
+from Bio.Align.Applications import MafftCommandline                                                                                 
+from Bio import AlignIO
+from Bio import Phylo 
+from Bio.Phylo.TreeConstruction import DistanceCalculator 
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor 
+
+import pylab
+
 from Bio import Entrez  
 from Bio import pairwise2  
 from Bio import SeqIO  
 from Bio.SubsMat import MatrixInfo as matlist  
-import time
+import time  
 from tqdm import tqdm 
  
+start_time = time.time()  
 Entrez.email = "Your.Name.Here@example.org"  
-handle = Entrez.esearch(db="nucleotide", retmax=3, term="txid2697049[Organism] and complete")  
+handle = Entrez.esearch(db="nucleotide", retmax=1000, term="txid2697049[Organism] and complete")  
 record = Entrez.read(handle)  
 handle.close()  
-SeqAll = []  
+my_alignments = [] #align1, align2, align3]
 print("Number of record ", len(record["IdList"]))  
 for IdSeq in record["IdList"]:  
     handle = Entrez.efetch(db="nucleotide", id=IdSeq, rettype="gb", retmode="text")  
-    record = SeqIO.read(handle, "genbank");  
+    record = SeqIO.read(handle, "genbank");
+    align1 = MultipleSeqAlignment([ SeqRecord(record.seq, id=record.id) ])
+                                                                                                                                       
+    my_alignments.append(align1)
+
     print("Size of Sequence ", IdSeq," ", len(record.seq))  
-    SeqAll.append(record)  
-  
-start_time = time.time()  
-matrix = matlist.blosum62  
-#alignments = pairwise2.align.globalxx(SeqAll[0].seq, SeqAll[2].seq)  
-for i in tqdm(range(1)): 
-    alignments = pairwise2.align.globaldx(SeqAll[0].seq, SeqAll[2].seq,matrix)  
-print("--- %s seconds ---" % (time.time() - start_time))                                                                                 
-
-########### RESULTS ###############
-  
-#Number of record  3
-#Size of Sequence  1827919722   29870
-#Size of Sequence  1827919707   29851
-#Size of Sequence  1827910768   29882
-#  0                                                                                                           | 0/1 [00:00<?, ?it/s]
-
-#100%|███████████████████████████████████████████████████████████████████████████████████████████████████| 1/1 [09:03<00:00, 543.90s/it]
-#--- 543.9143180847168 seconds ---
 
 
 
-#start_time = time.time()  
-#   ...: matrix = matlist.blosum62  
-#   ...: #alignments = pairwise2.align.globalxx(SeqAll[0].seq, SeqAll[2].seq)  
-#   ...: for i in tqdm(range(1)): 
-#   ...:     alignments02 = pairwise2.align.globaldx(SeqAll[0].seq, SeqAll[2].seq,matrix)  
-#   ...: print("--- %s seconds ---" % (time.time() - start_time))        
-#
-#
-#
+ 
 #align1 = MultipleSeqAlignment( 
-#    ...:     [ 
-#    ...:         SeqRecord(Seq(alignments01[0][0],generic_dna), id="Alpha"), 
-#    ...:         SeqRecord(Seq(alignments01[1][0], generic_dna), id="Beta"), 
-#    ...:         SeqRecord(Seq(alignments02[0][0][0:len(alignments01[0][0])], generic_dna), id="Gamma"), 
-#    ...:     ] 
-#    ...: ) 
-#
-#
-#calculator = DistanceCalculator('identity') 
-#    ...:                                                                                                                                           
-#
-#In [66]: dm = calculator.get_distance(align1) 
-#    ...:  
-#    ...: # Print the distance Matrix 
-#    ...: print('\nDistance Matrix\n===================') 
-#    ...: print(dm)                                                                                                                                 
-#
-#Distance Matrix
-#===================
-#Alpha   0
-#Beta    0.0     0
-#Gamma   0.649745683308794       0.649745683308794       0
-#        Alpha   Beta    Gamma
+#    [ SeqRecord(SeqAll[i].seq, id=SeqAll[i].id)  for i in range(2,10) ] 
+        #)                                        
+#my_alignments = [align1, align2, align3]
+
+AlignIO.write(my_alignments, "my_example.fasta", "fasta")
+
+in_file   = "my_example.fasta" 
+
+
+print('\nStarting MAFFT\n===================')  
+mafft_cline = MafftCommandline(input=in_file, thread=25) 
+stdout, stderr = mafft_cline() 
+with open("aligned.fasta", "w") as handle: 
+    handle.write(stdout) 
+from Bio import AlignIO 
+ 
+print('\nMAFFT Ended !!! \n===================')  
+
+aln = AlignIO.read("aligned.fasta", "fasta") 
+
+calculator = DistanceCalculator('identity') 
+
+dm = calculator.get_distance(aln) 
+  
+# Print the distance Matrix 
+print('\nDistance Matrix\n===================') 
+print(dm)                                                                                                                                
+
+# Construct the phylogenetic tree using UPGMA algorithm 
+constructor = DistanceTreeConstructor() 
+tree = constructor.upgma(dm) 
+ 
+# Draw the phylogenetic tree 
+Phylo.draw(tree) 
+ 
+# Print the phylogenetic tree in the terminal 
+print('\nPhylogenetic Tree\n===================') 
+Phylo.draw_ascii(tree)
+Phylo.draw(tree)
+pylab.savefig('apaf.png')
+print("--- %s seconds ---" % (time.time() - start_time))
+# 348
+#--- 1466.3146817684174 seconds ---
+
+
+
+>>> import sys
+>>> Phylo.write(tree, sys.stdout, "phyloxml")
